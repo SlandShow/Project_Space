026ad6d802effe7e3fb69f7ecdc30ef8b21040f4
Yamamura <SlandShow12345@mail.ru>
2016-11-25 23:40:14 +0300

    Создана система тайлов на основе Tile Map Editor

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -6,32 +6,38 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.mygdx.game.GameLogic.GameLoader;
+import com.mygdx.game.GameLogic.GameState;
 import com.mygdx.game.Screens.PlayScreen;
 
+/* Start all game here */
 public class MyGdxGame extends Game {
-	public SpriteBatch batch;
-	Texture img;
-	public static final int V_WIDTH = 400;
-	public static final int V_HEIGHT = 208;
-	public static final float PPM = 100;
+    public static SpriteBatch batch;
+    Texture img;
+    public static final int V_WIDTH = 400;
+    public static final int V_HEIGHT = 208;
+    public static final float PPM = 100;
+    private GameLoader load;
 
 
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        load = new GameLoader(this);
+        setScreen(load.getCurrentState());
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		setScreen(new PlayScreen(this));
+    }
 
-	}
+    @Override
+    public void render() {
+        super.render();
+    }
 
-	@Override
-	public void render () {
-		super.render();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
+
 }

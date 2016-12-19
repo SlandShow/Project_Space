582f4b81d2c60890deb1f1811de863d1c78e29ca
Yamamura <SlandShow12345@mail.ru>
2016-12-19 21:57:33 +0300

    Добавлены логи для мониторинга приложения

--- core/src/com/mygdx/game/Tools/WorldContactListener.java
+++ core/src/com/mygdx/game/Tools/WorldContactListener.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.Tools;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
@@ -11,12 +12,14 @@ import com.badlogic.gdx.physics.box2d.Manifold;
 public class WorldContactListener implements ContactListener {
     @Override
     public void beginContact(Contact contact) {
-
+      // мониторинг с помощью логов
+        Gdx.app.log("Begin contact", "");
     }
 
     @Override
     public void endContact(Contact contact) {
-
+        // мониторинг с помощью логов
+        Gdx.app.log("End contact", "");
     }
 
     @Override

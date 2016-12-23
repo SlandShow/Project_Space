71776260a322f4c316114a212cf4e702fd071386
Yamamura <SlandShow12345@mail.ru>
2016-12-23 21:24:10 +0300

    Добавлен новый метод распознания коллизии

--- core/src/com/mygdx/game/Tools/WorldContactListener.java
+++ core/src/com/mygdx/game/Tools/WorldContactListener.java
@@ -1,15 +1,16 @@
 package com.mygdx.game.Tools;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.physics.box2d.Contact;
-import com.badlogic.gdx.physics.box2d.ContactImpulse;
-import com.badlogic.gdx.physics.box2d.ContactListener;
-import com.badlogic.gdx.physics.box2d.Manifold;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.physics.box2d.*;
+
 
 /**
  * Created by Admin on 19.12.2016.
  */
 public class WorldContactListener implements ContactListener {
+
     @Override
     public void beginContact(Contact contact) {
       // мониторинг с помощью логов
@@ -30,5 +31,10 @@ public class WorldContactListener implements ContactListener {
     @Override
     public void postSolve(Contact contact, ContactImpulse impulse) {
 
+    }
+
+    // использовать только для Rectangle!
+    public static boolean checkRectangleCollision(Rectangle s1, Rectangle s2, Rectangle s3) {
+        return Intersector.intersectRectangles(s1, s2, s3);
     }
 }

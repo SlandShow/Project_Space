d47f0a131355b255901cca5b93ee48bd1dce7a9e
Yamamura <SlandShow12345@mail.ru>
2016-12-23 15:13:39 +0300

    Небольшая чистка кода

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,24 +1,33 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
 import com.mygdx.game.GameLogic.GameLoader;
-import com.mygdx.game.GameLogic.GameState;
-import com.mygdx.game.Screens.PlayScreen;
+
 
 /* Start all game here */
 public class MyGdxGame extends Game {
+
     public static SpriteBatch batch;
-    Texture img;
     public static final int V_WIDTH = 400;
     public static final int V_HEIGHT = 208;
     public static final float PPM = 100;
     private GameLoader load;
+
+    //Box2D Collision Bits
+    public static final short NOTHING_BIT = 0;
+    public static final short GROUND_BIT = 1;
+    public static final short MARIO_BIT = 2;
+    public static final short BRICK_BIT = 4;
+    public static final short COIN_BIT = 8;
+    public static final short DESTROYED_BIT = 16;
+    public static final short OBJECT_BIT = 32;
+    public static final short ENEMY_BIT = 64;
+    public static final short ENEMY_HEAD_BIT = 128;
+    public static final short ITEM_BIT = 256;
+    public static final short MARIO_HEAD_BIT = 512;
+    public static final short FIREBALL_BIT = 1024;
 
 
     @Override
@@ -37,7 +46,6 @@ public class MyGdxGame extends Game {
     @Override
     public void dispose() {
         batch.dispose();
-        img.dispose();
     }
 
 }

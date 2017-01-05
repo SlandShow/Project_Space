25e1d2875655ced8d352a5c0e29b40068009178f
Yamamura <SlandShow12345@mail.ru>
2017-01-05 20:04:28 +0300

    Исправление ошибок, которые появлялись из-за Box2D

--- core/src/com/mygdx/game/Tools/B2WorldCreater.java
+++ core/src/com/mygdx/game/Tools/B2WorldCreater.java
@@ -4,9 +4,12 @@ import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.Array;
+import com.mygdx.game.BulletsResurses.Box2DBullets;
 import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.Sprites.Player;
 import com.mygdx.game.Sprites.SimpleEnemy;
 
 /**
@@ -15,22 +18,65 @@ import com.mygdx.game.Sprites.SimpleEnemy;
 public class B2WorldCreater {
 
     public static Array<SimpleEnemy> enemies = new Array<SimpleEnemy>();
+    private BodyDef bdef = new BodyDef();
+    private PolygonShape shape = new PolygonShape();
+    private FixtureDef fdef = new FixtureDef();
+    private Body body;
+
+    public World getWorld() {
+        return world;
+    }
+
+    public Body getBody() {
+        return body;
+    }
+
+    public FixtureDef getFdef() {
+        return fdef;
+    }
+
+    public PolygonShape getShape() {
+        return shape;
+    }
+
+    public BodyDef getBdef() {
+        return bdef;
+    }
+
+    private World world;
 
     public B2WorldCreater(World world, TiledMap map) {
 
-        BodyDef bdef = new BodyDef();
-        PolygonShape shape = new PolygonShape();
-        FixtureDef fdef = new FixtureDef();
-        Body body;
+       this.world = world;
 
-
-
-
-
-        
-        // create ground bodies/fixtures (polygon)
+        // create ground bodies/fixtures (polygon) - платформы
         for (MapObject obj :
                 map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) obj).getRectangle();
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(
+                    (rect.getX() + rect.getWidth() / 2) / MyGdxGame.PPM,
+                    (rect.getY() + rect.getHeight() / 2) / MyGdxGame.PPM);
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2 / MyGdxGame.PPM, rect.getHeight() / 2 / MyGdxGame.PPM);
+            fdef.shape = shape;
+            body.createFixture(fdef).setUserData("platform");
+        }
+
+
+        // враги
+        for (MapObject obj :
+                map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) obj).getRectangle();
+            enemies.add(new SimpleEnemy(world, bdef, shape, rect, fdef));
+
+        }
+
+
+
+        for (MapObject obj :
+                map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)) {
             Rectangle rect = ((RectangleMapObject) obj).getRectangle();
             bdef.type = BodyDef.BodyType.StaticBody;
             bdef.position.set(
@@ -43,13 +89,29 @@ public class B2WorldCreater {
             body.createFixture(fdef);
         }
 
-
         for (MapObject obj :
-                map.getLayers().get(6).getObjects().getByType(RectangleMapObject.class)) {
+                map.getLayers().get(1).getObjects().getByType(RectangleMapObject.class)) {
             Rectangle rect = ((RectangleMapObject) obj).getRectangle();
-            enemies.add(new SimpleEnemy(world, bdef, shape, rect, fdef));
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(
+                    (rect.getX() + rect.getWidth() / 2) / MyGdxGame.PPM,
+                    (rect.getY() + rect.getHeight() / 2) / MyGdxGame.PPM);
+            body = world.createBody(bdef);
 
+            shape.setAsBox(rect.getWidth() / 2 / MyGdxGame.PPM, rect.getHeight() / 2 / MyGdxGame.PPM);
+            fdef.shape = shape;
+            body.createFixture(fdef);
         }
+
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        bdef.position.set(1, 1);
+        body = world.createBody(bdef);
+        shape.setAsBox(1 / MyGdxGame.PPM, 1 / MyGdxGame.PPM);
+        fdef.shape = shape;
+        bdef.bullet = true;
+        body.createFixture(fdef).setUserData("bullet");
+        body.applyLinearImpulse(new Vector2(4f, 0f), new Vector2(4f, 0f), true);
+
 
  /*
         // create pipe bodies
@@ -90,7 +152,17 @@ public class B2WorldCreater {
         }*/
 
 
+    }
 
+    public void update(float dt) {
+       /* bdef.type = BodyDef.BodyType.DynamicBody;
+        bdef.position.set(0.5f, 0.5f);
+        body = world.createBody(bdef);
+        shape.setAsBox(1 / MyGdxGame.PPM, 1 / MyGdxGame.PPM);
+        fdef.shape = shape;
+        bdef.bullet = true;
+        body.createFixture(fdef).setUserData("bullet");
+        body.applyLinearImpulse(new Vector2(4f, 0f), new Vector2(4f, 0f), true);*/
     }
 
     public void dispose() {

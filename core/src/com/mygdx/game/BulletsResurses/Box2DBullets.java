72a20814cc7cbb76dd08b0beae0eded1b2be0826
Yamamura <SlandShow12345@mail.ru>
2017-01-06 17:43:16 +0300

    Работа на пулями

--- core/src/com/mygdx/game/BulletsResurses/Box2DBullets.java
+++ core/src/com/mygdx/game/BulletsResurses/Box2DBullets.java
@@ -1,6 +1,8 @@
 package com.mygdx.game.BulletsResurses;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
@@ -22,9 +24,20 @@ public class Box2DBullets extends Sprite {
     private float r;
     private Vector2 velocity;
     private float velocity_x;
+    private float velocity_y;
     private boolean bulletTurnLeft;
     private boolean bulletTurnRight;
+    private boolean checkTurning;
+    private boolean factingLeft;
+    private boolean factingRight;
 
+    public void setFactingLeft(boolean factingLeft) {
+        this.factingLeft = factingLeft;
+    }
+
+    public void setFactingRight(boolean factingRight) {
+        this.factingRight = factingRight;
+    }
 
     public Box2DBullets(B2WorldCreater creater, float x, float y) {
         this.world = creater.getWorld();
@@ -32,9 +45,11 @@ public class Box2DBullets extends Sprite {
         this.shape = creater.getShape();
         this.fdef = creater.getFdef();
         this.b2body = creater.getBody();
-        this.x = x;
-        this.y = y;
-        velocity_x = 4f;
+        this.x = x ;
+        this.y = y + 20 / MyGdxGame.PPM;
+        velocity_x = 5f;
+        velocity_y = 400f;
+        checkTurning = false;
         defineBullet();
     }
 
@@ -61,25 +76,35 @@ public class Box2DBullets extends Sprite {
         bdef.type = BodyDef.BodyType.DynamicBody;
         bdef.position.set(x, y);
         b2body = world.createBody(bdef);
-        shape.setAsBox(2 / MyGdxGame.PPM, 2 / MyGdxGame.PPM);
+        shape.setAsBox(1 / MyGdxGame.PPM, 1 / MyGdxGame.PPM);
         fdef.shape = shape;
         bdef.bullet = true;
-
+        b2body.setGravityScale(0);
         b2body.createFixture(fdef).setUserData("bullet");
+        factingRight = false; // по умолчанию
 
 
     }
 
     public void update(float dt) {
 
-        if (bulletTurnLeft) b2body.applyLinearImpulse(new Vector2(-velocity_x, 0), new Vector2(velocity_x, 0), false);
-        else b2body.applyLinearImpulse(new Vector2(velocity_x, 0), b2body.getWorldCenter(), false);
-        
+        if (checkTurning == false) {
+            b2body.applyLinearImpulse(new Vector2(velocity_x, 0), b2body.getWorldCenter(), true);
+            if (factingRight) b2body.applyLinearImpulse(new Vector2(velocity_x, 0), b2body.getWorldCenter(), true);
+            if (factingLeft) b2body.applyLinearImpulse(new Vector2(-velocity_x, 0), b2body.getWorldCenter(), true);
+            checkTurning = true;
+        }
+
+
+
     }
 
     public void kill() {
-
+        world.destroyBody(b2body);
     }
+
+
+
 
 
 }
